c51939c57d4bf0cbff5d866d261d32b983d61233
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-04-25 23:21:33 -0500

    initial video schema

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config();
+console.log(process.env.HELLO)
+
 export default {
-    MONGO_DATABASE: 'videos-database',
-    MONGO_USER: 'admin',
-    MONGO_PASSWORD: 'admin',
-    MONGO_HOST: 'localhost'
+    MONGO_DATABASE: process.env.MONGO_DATABASE || 'videos-database',
+    MONGO_USER: process.env.MONGO_USER ||  'admin',
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD ||  'admin',
+    MONGO_HOST: process.env.MONGO_HOST ||  'localhost'
 }
